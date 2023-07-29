e7bc6e2b755fff530e669eb212110b955117e39a
itaigolan <itai.golan@hotmail.com>
2023-07-28 19:33:37 -0700

    Implement Data processing

--- app.py
+++ app.py
@@ -8,35 +8,37 @@ app = Flask(__name__)
 
 def etl():
     # Load CSV files and parse data
-    user_list = parse_users("data/users.csv")
+    user_table = parse_users("data/users.csv")
     experiment_list = parse_experiments("data/user_experiments.csv")
-    compound_list = parse_compounds("data/compounds.csv")
+    compound_table = parse_compounds("data/compounds.csv")
+
+    # Associate each experiment with the user that ran it
+    for experiment in experiment_list:
+       user_table[experiment.user_id].add_experiment(experiment)
 
     # Process files to derive features
     # Upload processed data into a database
     pass
 
 def parse_users(data_source):
-    user_list = []
     users_file = open(data_source, newline='')
     users_reader = csv.reader(users_file, delimiter=',')
     next(users_reader)
+    user_table= {}
     for row in users_reader:
-        user_list.append(
-            User(
+        user_table[int(row[0])] =  User(
                 user_id=row[0],
                 name=row[1],
                 email=row[2],
                 signup_date=row[3]
             )
-        )
-    return user_list
+    return user_table
 
 def parse_experiments(data_source):
-    experiment_list = []
     experiments_file = open(data_source, newline='')
     experiments_reader = csv.reader(experiments_file, delimiter=',')
     next(experiments_reader)
+    experiment_list = []
     for row in experiments_reader:
         experiment_list.append(
             Experiment(
@@ -49,19 +51,17 @@ def parse_experiments(data_source):
     return experiment_list
 
 def parse_compounds(data_source):
-    compound_list = []
     compound_file = open(data_source, newline='')
     compound_reader = csv.reader(compound_file, delimiter=',')
     next(compound_reader)
+    compound_table = {}
     for row in compound_reader:
-        compound_list.append(
-            Compound(
+        compound_table[int(row[0])] = Compound(
                 compound_id=row[0],
                 compound_name=row[1],
                 compound_structure=row[2]
             )
-        )
-    return compound_list
+    return compound_table
 
 @app.route("/", methods=["POST"])
 def trigger_etl():
--- user.py
+++ user.py
@@ -1,11 +1,38 @@
+from experiment import Experiment
+
 class User:
     user_id: int
     name: str
     email: str
     signup_date:str
+    experiments: [Experiment]
 
     def __init__(self, user_id, name, email, signup_date):
         self.user_id = int(user_id)
         self.name = name.strip()
         self.email = email.strip()
         self.signup_date = signup_date.strip()
+        self.experiments = []
+
+    def add_experiment(self, experiment):
+        self.experiments.append(experiment)
+
+    def total_experiments_ran(self):
+        return len(self.experiments)
+    
+    def average_experiment_duration(self):
+        total_duration = 0
+        for experiment in self.experiments:
+            total_duration += experiment.experiment_run_time
+        
+        return float(total_duration/self.total_experiments_ran())
+    
+    def most_common_compound(self):
+        compounds = {}
+        for experiment in self.experiments:
+            for compound in experiment.experiment_compound_ids:
+                if compound in compounds.keys():
+                    compounds[compound] = compounds[compound] + 1
+                else:
+                    compounds[compound] = 1
+        return max(compounds, key=compounds.get)
